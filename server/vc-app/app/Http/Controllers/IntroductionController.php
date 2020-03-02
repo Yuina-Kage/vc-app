fdbc7cb959dc33945e741a1e14ca190354bb2225
Yuina-Kage <yuina.kg@gmail.com>
2020-03-02 12:48:43 +0900

    自分の名前完成2

---
+++ server/vc-app/app/Http/Controllers/IntroductionController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IntroductionController extends Controller
+{
+    
+    public function name()
+    {
+	return view('introduction.name', ['msg' => '鹿毛唯菜です！！']);
+    }
+}
+
